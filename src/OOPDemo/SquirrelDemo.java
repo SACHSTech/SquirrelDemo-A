b1b4c6c1700bf4e8edb615dc9181fb765d243d90
mrfabroa <eric.fabroa@ycdsbk12.ca>
2022-10-19 09:42:35 -0400

    SquirrelDemo complete

--- src/OOPDemo/SquirrelDemo.java
+++ src/OOPDemo/SquirrelDemo.java
@@ -1,5 +1,32 @@
 package OOPDemo;
 
+import java.beans.Beans;
+
+import OOPDemo.Squirrel;
+
 public class SquirrelDemo {
+
+    public static void main(String[] args) {
+        Squirrel walterSquirrel;
+        Squirrel jesseSquirrel;
+        Squirrel beanSquirrel;
+        Squirrel alvinSquirrel;
+
+        walterSquirrel = new Squirrel(20.3, 10.5, "brown", "Brown", "Walter");
+        jesseSquirrel = new Squirrel(25.1, 2.6, "Gray", "Western Gray Squirrel", "Jesse");
+        beanSquirrel = new Squirrel(43.8, 15.1, "Black", "Black Squirrel", "Beans");
+        alvinSquirrel = new Squirrel(12.3, 5.2 , "Brown", "Chipmunk", "Alvin");
+
+
+        walterSquirrel.walk();
+        jesseSquirrel.storeNuts();
+        beanSquirrel.scramble();
+        alvinSquirrel.injury();
+
+        System.out.println("Alvin's Health:" + alvinSquirrel.getHealth());
+
+    }
+
+
     
 }
